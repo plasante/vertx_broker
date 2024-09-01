fbf11162cfed0afcec8fa84bc687b17a1b6b5b5e
Pierre Lasante <pierre.lasante@videotron.ca>
2024-09-01 07:06:39 -0400

    Second Unit Test TestQuotesRestApi.java part 2

--- src/main/java/com/uniksoft/broker/quotes/QuotesRestApi.java
+++ src/main/java/com/uniksoft/broker/quotes/QuotesRestApi.java
@@ -1,17 +1,24 @@
 package com.uniksoft.broker.quotes;
 
 import com.uniksoft.broker.assets.Asset;
+import com.uniksoft.broker.assets.AssetsRestApi;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class QuotesRestApi {
   private static final Logger LOG = LogManager.getLogger(QuotesRestApi.class);
 
   public static void attach(Router parent) {
+    final Map<String, Quote> cachedQuotes = new HashMap<>();
+    AssetsRestApi.ASSETS.forEach(symbol ->
+      cachedQuotes.put(symbol, initRandomQuote(symbol))
+    );
 
     // Create an HTTP end point with path /quotes
     // :assets is a path parameter i.e. /quotes/AAPL
@@ -21,7 +28,7 @@ public class QuotesRestApi {
       LOG.info("Received request for asset: {}", assetParam);
 
       // We return a fictitious asset
-      var quote = initRandomQuote(assetParam);
+      var quote = cachedQuotes.get(assetParam);
 
       final JsonObject response = quote.toJsonObject();
       LOG.info("Path {} responds with {}", routingContext.normalisedPath(), response.encodePrettily());
